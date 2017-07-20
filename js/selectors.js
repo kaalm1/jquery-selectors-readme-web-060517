593d44f03f571583d0ffcd237726731b8ec069d9
kaalm1 <kaalm1@gmail.com>
2017-07-20 10:20:00 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,23 @@
 // declare your functions here...
+// declare your functions here...
+function paragraphSelector(){
+ return $('p')
+}
+
+function lastImageSelector(){
+ let elem = $('img')
+ return elem.slice(-1)
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
+
+function divSelector(){
+  let elem = $('div')
+  return elem.slice(0,2)
+}
+
+function firstListItem(){
+  return $('ul li').slice(0,1)
+}
